9a55ccb3437aec4227eea61a14e3c9ef52f373b5
Rapix27 <rapix24052011@gmail.com>
2026-02-26 09:27:55 -0300

    pegamos os dados

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("Eu funcionei")
+const numero1 =  document.getElementById("numero1")
+const numero2 = document.getElementById("numero2")
+const button =  document.getElementById("button")
+
+function calculaMedia(){
+    const nota1 = Number(numero1.value) 
+    const nota2 = Number(numero2.value)
+
+    console.log(nota1 + nota2)
+}
+
+button.addEventListener("click", calculaMedia)
